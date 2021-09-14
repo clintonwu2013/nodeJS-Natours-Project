643d5fd3253ffa0243abf71928a88d28615b23b2
吳宗霖 <clintonwu2013@gmail.com>
2021-09-14 23:17:13 +0800

    add insert tour

--- app.js
+++ app.js
@@ -1,27 +1,53 @@
-const express = require('express');
-const fs = require('fs');
-const app = express();
-const port = 8080;
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, "utf-8"));
+const express = require('express')
+const fs = require('fs')
+const app = express()
+const port = 8080
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf-8'),
+)
+
+app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.status(200).json({ message: "hello~~~" })
+  res.status(200).json({ message: 'hello~~~' })
 })
 
 app.post('/', (req, res) => {
-    res.status(200).json({ message: "post hello" })
+  res.status(200).json({ message: 'post hello' })
 })
 
 app.get('/api/v1/tours', (req, res) => {
-    res.status(200).json({
-        status: "success",
-        result: tours.length,
+  res.status(200).json({
+    status: 'success',
+    result: tours.length,
+    data: {
+      tours: tours,
+    },
+  })
+})
+
+app.post('/api/v1/tours', (req, res) => {
+  //console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1
+  const newTour = Object.assign({ id: newId }, req.body)
+  tours.push(newTour)
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      console.log(err)
+      res.status(201).json({
+        status: 'success',
         data: {
-            tours: tours
-        }
-    })
+          tour: newTour,
+        },
+      })
+    },
+  )
+  //res.send("done");
 })
 
 app.listen(port, () => {
-    console.log(`App is running on port ${port}`)
+  console.log(`App is running on port ${port}`)
 })
