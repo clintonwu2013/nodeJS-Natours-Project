949ba2aad8759d1e814db34d58e2c93978c46170
吳宗霖 <clintonwu2013@gmail.com>
2021-09-14 22:39:28 +0800

    add starter files

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
-
+const fs = require('fs');
 const app = express();
-
 const port = 8080;
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, "utf-8"));
 
 app.get('/', (req, res) => {
     res.status(200).json({ message: "hello~~~" })
@@ -11,6 +11,17 @@ app.get('/', (req, res) => {
 app.post('/', (req, res) => {
     res.status(200).json({ message: "post hello" })
 })
+
+app.get('/api/v1/tours', (req, res) => {
+    res.status(200).json({
+        status: "success",
+        result: tours.length,
+        data: {
+            tours: tours
+        }
+    })
+})
+
 app.listen(port, () => {
     console.log(`App is running on port ${port}`)
 })
