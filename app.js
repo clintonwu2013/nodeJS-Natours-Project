448610a26e60cb587676c21f1be6948e4d3004e5
吳宗霖 <clintonwu2013@gmail.com>
2021-09-14 23:37:36 +0800

    add get single tour api

--- app.js
+++ app.js
@@ -26,6 +26,26 @@ app.get('/api/v1/tours', (req, res) => {
   })
 })
 
+app.get('/api/v1/tours/:id', (req, res) => {
+  console.log(req.params)
+  const id = req.params.id * 1
+  const tour = tours.find((el) => el.id === id)
+  if (!tour) {
+    res.status(404).json({
+      status: 'fail',
+      message: 'invalid id',
+    })
+    return
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: tour,
+    },
+  })
+})
+
 app.post('/api/v1/tours', (req, res) => {
   //console.log(req.body);
   const newId = tours[tours.length - 1].id + 1
