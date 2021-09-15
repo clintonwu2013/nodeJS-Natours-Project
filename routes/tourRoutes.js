e2ed0f98b537c9b2b2915d8c0f084e4344d1f3bd
timowu <timowu@changingtec.com>
2021-09-15 17:50:11 +0800

    follow eslint format

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -5,8 +5,14 @@ const tourController = require("./../controllers/tourController");
 
 router.param("id", tourController.checkID);
 
-router.route("/").
-get(tourController.getAllTours).post(tourController.checkNameAndPrice, tourController.createTour);
-router.route("/:id").get(tourController.getTour).patch(tourController.modifyTour).delete(tourController.deleteTour);
+router
+  .route("/")
+  .get(tourController.getAllTours)
+  .post(tourController.checkNameAndPrice, tourController.createTour);
+router
+  .route("/:id")
+  .get(tourController.getTour)
+  .patch(tourController.modifyTour)
+  .delete(tourController.deleteTour);
 
 module.exports = router;
