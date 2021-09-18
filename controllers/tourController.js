dcba332a67ee407cf555b9c4d2ad03351574b5ae
吳宗霖 <clintonwu2013@gmail.com>
2021-09-18 20:49:35 +0800

    add tour model and filter methodsd

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,8 +2,17 @@ const Tour = require("./../models/tourModel");
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    const queryObj = { ...req.query };
+    const excludedFields = ["page", "sort", "limit", "fields"];
+    excludedFields.forEach(el => delete queryObj[el]);
+    const query = Tour.find(queryObj);
 
+    // const query = await Tour.find()
+    //   .where("duration")
+    //   .equals(5)
+    //   .where("difficulty")
+    //   .equals("easy");
+    const tours = await query;
     res.status(200).json({
       status: "success",
       result: tours.length,
