c1c9e8d9acb4bdb1cff54517362427d3072d1959
吳宗霖 <clintonwu2013@gmail.com>
2021-09-20 09:58:43 +0800

    add aggregation features

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -3,6 +3,8 @@ const express = require("express");
 const router = express.Router();
 const tourController = require("./../controllers/tourController");
 
+router.route("/get-stats").get(tourController.getTourStats);
+router.route("/monthly-plan/:year").get(tourController.getMonthlyPlan);
 router
   .route("/top-5-cheap")
   .get(tourController.aliaseTopTours, tourController.getAllTours);
