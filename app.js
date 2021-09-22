a4f51863d5fc4d7307bff40365529e137cc29ee7
吳宗霖 <clintonwu2013@gmail.com>
2021-09-22 22:09:51 +0800

    add error handling

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require("express");
 
 const app = express();
 const morgan = require("morgan");
-
+const AppError = require("./utils/appError");
+const globalErrorHandler = require("./controllers/errorController");
 const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
 
@@ -10,25 +11,19 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 app.use(express.json());
-app.use((req, res, next) => {
-  console.log("hello from the middleware");
-  next();
-});
+
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
 
-app.use(express.static(`${__dirname}/public`));
-app.get("/", (req, res) => {
-  res.status(200).json({ message: "hello~~~" });
-});
-
-app.post("/", (req, res) => {
-  res.status(200).json({ message: "post hello" });
-});
-
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
+
+app.all("*", (req, res, next) => {
+  next(new AppError(`cannot find ${req.originalUrl} on the server`, 404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
