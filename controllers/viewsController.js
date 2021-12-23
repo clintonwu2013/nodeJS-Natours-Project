e7f2333a58631b206a70d85d334fef7bb90b8dc7
吳宗霖 <clintonwu2013@gmail.com>
2021-12-23 22:11:43 +0800

    add mapbox

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -9,8 +9,22 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getTour = (req, res) => {
-  res.status(200).render("tour", {
-    title: "the forest hiker tour"
+exports.getTour = catchAsync(async (req, res, next) => {
+  //console.log(req.params.slug);
+  const { slug } = req.params;
+  const tour = await Tour.findOne({ slug: slug }).populate({
+    path: "reviews",
+    fields: "review rating user"
   });
-};
+  console.log(tour);
+  res
+    .status(200)
+    .set(
+      "Content-Security-Policy",
+      "default-src 'self' https://*.mapbox.com ;base-uri 'self';block-all-mixed-content;font-src 'self' https: data:;frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src https://cdnjs.cloudflare.com https://api.mapbox.com 'self' blob: ;script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests;"
+    )
+    .render("tour", {
+      title: `${tour.name} tour`,
+      tour: tour
+    });
+});
---
+++ public/js/mapbox.js
@@ -0,0 +1,33 @@
+/* eslint-disabled*/
+
+const locations = JSON.parse(document.getElementById("map").dataset.locations);
+console.log(locations);
+mapboxgl.accessToken =
+  "pk.eyJ1IjoidGltb3d1IiwiYSI6ImNreGowbG9rNjBnMTYyeG81NGx0Zmh6dmQifQ.4qRV4KQxZ3z8yxvChotV-A";
+const map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/timowu/ckxj0v0vakaij14pgjddbpbc1"
+});
+
+const bounds = new mapboxgl.LngLatBounds();
+
+locations.forEach(loc => {
+  const el = document.createElement("div");
+  el.className = "marker";
+  new mapboxgl.Marker({
+    element: el,
+    anchor: "bottom"
+  })
+    .setLngLat(loc.coordinates)
+    .addTo(map);
+
+  new mapboxgl.Popup({
+    offSet: 30
+  })
+    .setLngLat(loc.coordinates)
+    .setHTML(`<P>Day ${loc.day}: ${loc.description}</p>`)
+    .addTo(map);
+  bounds.extend(loc.coordinates);
+});
+
+map.fitBounds(bounds);
